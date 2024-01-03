c281f9b900bd54f93c923b4b27295513a2a97c0c
ahmad-mukhlish <ahmad.mukhlish.saputra@gmail.com>
2024-01-03 11:40:53 +0700

    init select command

--- main.go
+++ main.go
@@ -1,15 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/jackc/pgx/v5/stdlib"
+	"log"
+)
 
 func main() {
-	fmt.Println("Hello World")
 
 	//connect to the database
+	db, err := sql.Open("pgx", "host=localhost port=54321 dbname=test user=ahmadmukhlis password=password")
+	if err != nil {
+		log.Fatal("error", err)
+	}
+	defer db.Close()
 
 	//ping the database
+	err = db.Ping()
+	if err != nil {
+		log.Fatal("error", err)
+	}
 
 	//show the database content
+
+	err = getRows(db)
+	if err != nil {
+		log.Fatal("error", err)
+	}
 
 	//insert a row to a database
 
@@ -23,4 +41,35 @@ func main() {
 
 	//show the database content
 
+}
+
+func getRows(db *sql.DB) error {
+
+	selectQuery := `select id, first_name, last_name from users`
+
+	rows, err := db.Query(selectQuery)
+	if err != nil {
+		log.Fatal("error", err)
+	}
+	defer rows.Close()
+
+	var firstName, lastName string
+	var id int
+
+	if err != nil {
+		return err
+	}
+
+	for rows.Next() {
+		err = rows.Scan(&id, &firstName, &lastName)
+		fmt.Printf("Row %d, %s %s \n", id, firstName, lastName)
+	}
+
+	if err = rows.Err(); err != nil {
+		log.Fatal("error", err)
+	}
+
+	fmt.Println("-----------------")
+
+	return nil
 }
