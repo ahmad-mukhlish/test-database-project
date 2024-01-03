e2513f7918f7df09e8703d87d3271157075d2aee
ahmad-mukhlish <ahmad.mukhlish.saputra@gmail.com>
2024-01-03 11:18:14 +0700

    init project

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello World")
+
+	//connect to the database
+
+	//ping the database
+
+	//show the database content
+
+	//insert a row to a database
+
+	//show the database content
+
+	//update a row in the database
+
+	//show the database content
+
+	//delete a row in the database
+
+	//show the database content
+
+}
